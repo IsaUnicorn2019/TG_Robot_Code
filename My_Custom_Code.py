184877a6322c99940c6796cbeda17596a5eaa13a
IsaUnicorn2019 <isa@abslink.net>
2019-12-11 00:29:51 +0000

    change

--- My_Custom_Code.py
+++ My_Custom_Code.py
@@ -14,14 +14,14 @@ def my_custom_autonomous(hat):
 
    print(nr_balls)
         #auto.forward(0.2, 0.5)
-        auto.turn_right(1, 5)
+        auto.turn_right(0, -0)
         auto.forward(0.8,2)
-        auto.turn_left(1,5)
-        auto.forward(0.8,2)
-        auto.turn_left(1,5)
-        auto.forward(0.8,2)
-        auto.turn_right(1,5)
-    nr_balls = 0
+        #auto.turn_left(1,5)
+        #auto.forward(0.8,2)
+        #auto.turn_left(1,5)
+        #auto.forward(0.8,2)
+       # auto.turn_right(1,5)
+   # nr_balls = 0
     # first dispenser
    # while nr_balls < 10:
        # print(nr_balls)
@@ -48,55 +48,55 @@ def my_custom_autonomous(hat):
 
 def my_custom_teleop():
     #controller class
-    controller = Controller()
+   # controller = Controller()
 
     # initialize Pi Hat
-    hat = Py_Hat(address=96)
+   # hat = Py_Hat(address=96)
     
     while True:
-        controller.event_get()
+        #controller.event_get()
         # setup controls
-        leftstick = controller.set_axis('leftstick')
-        rightstick = controller.set_axis('rightstick')
-        LT = controller.set_axis('LT')
-        RT = controller.set_axis('RT')
+        #leftstick = controller.set_axis('leftstick')
+        #rightstick = controller.set_axis('rightstick')
+       # LT = controller.set_axis('LT')
+       # RT = controller.set_axis('RT')
 
         # Button press to run Autonomous
         
-        if LT > .75:
-            my_custom_autonomous(hat) 
-        if RT > .9:
+       # if LT > .75:
+           # my_custom_autonomous(hat) 
+        #if RT > .9:
             print("2")
-            hat.motor(0, -.4)
-            hat.motor(1, .2)
-            sleep(.3)
-            hat.motor(0, .4)
-            hat.motor(1, -.2)
-            sleep(.3)
+           #@ hat.motor(0, -.4)
+           # hat.motor(1, .2)
+           # sleep(.3)
+           # hat.motor(0, .4)
+           # hat.motor(1, -.2)
+           # sleep(.3)
 
 
         # # drivetrain examples
-        if leftstick > .05 or leftstick < -.05:
-            hat.motor(0, leftstick)
+       # if leftstick > .05 or leftstick < -.05:
+            #hat.motor(0, leftstick)
         else:
-            hat.motor(0, .05)   
+           # hat.motor(0, .05)   
             
 
-        if rightstick > .05 or rightstick < -.05:
-            hat.motor(1, -rightstick)
+       # if rightstick > .05 or rightstick < -.05:
+            #hat.motor(1, -rightstick)
         else:
-            hat.motor(1, .05)
+            #hat.motor(1, .05)
 
         # # sleep for smooth loops
-        sleep(.02)
+       # sleep(.02)
         
 
 
 if __name__ == "__main__":
-    import os
-    os.system("sudo pkill -9 -f RobotCode.py")
+    #import os
+    #os.system("sudo pkill -9 -f RobotCode.py")
     
-    my_custom_teleop()
+   # my_custom_teleop()
 
 
 
